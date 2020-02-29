d3cb90caf960acd64719149aff728ba70463910e
crybaby8 <ad8146@student.uni-lj.si>
2020-02-29 10:26:54 +0000

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,7 +4,9 @@ public class Gravitacija{
 	
 	public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ois je zakon");
+        int v = sc.nextInt();
+        System.out.println(((6.674E-11) * 
+        (5.972E24))/((6.371E6) + v * v));
         
 	}
 }
