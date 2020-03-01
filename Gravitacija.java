cc0661bc69cc50a1c16414e30196c49e3ec48c0c
crybaby8 <ad8146@student.uni-lj.si>
2020-03-01 13:03:36 +0000

    Popravki

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,9 +5,18 @@ public class Gravitacija{
 	public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int v = sc.nextInt();
-        System.out.println(((6.674E-11) * 
-        (5.972E24))/((6.371E6) + v * v));
-        System.out.println(v);
+        double g = gravitacija(v);
+        izpis(v, g);
+	}
+	
+	public static double gravitacija(int v) {
+	    return (((6.674E-11) * (5.972E24)) / ((6.371E6 + v) * (6.371E6 + v)));
+	}
+	
+	public static void izpis(int v, double gravitacija) {
+	    System.out.println(v);
+	    System.out.println(gravitacija);
+	    return;
 	}
 }
     
